ae2aca0e6934c3795b9dc21a26675f7f85b2ba9e
MathisPhg <mathis.phg@gmail.com>
2026-09-20 18:47:02 +0200

    feat: 1.3 Ajout des class de Prescription

---
+++ models/prescription.py
@@ -0,0 +1,69 @@
+from abc import ABC, abstractmethod
+
+class Prescription(ABC) :
+
+    def __init__(self, traitement: str, posologie: str, duree_traitement: str ) -> None:
+        self._traitement = traitement
+        self._posologie = posologie
+        self._duree_traitement = duree_traitement
+
+    @abstractmethod
+    def afficher_details(self) :
+        pass
+
+
+class PrescriptionMedicamenteuse(Prescription) :
+
+    def __init__(self, medicament: str, posologie: str, duree_traitement: str, dosage: str, frequence: str) -> None:
+        super().__init__(medicament, posologie, duree_traitement)
+        self._medicament = medicament
+        self._dosage = dosage
+        self._frequence = frequence
+
+    def afficher_details(self):
+        
+        return (
+            "Prescription medicamenteuse :\n"
+            f"- Medicament : {self._medicament}\n"
+            f"- Dosage : {self._dosage}\n"
+            f"- Fréquence : {self._frequence}\n"
+            f"- Posologie : {self._posologie}\n"
+            f"- Durée du traitement : {self._duree_traitement}\n"
+            )
+    
+class PrescriptionExamen(Prescription) :
+
+    def __init__(self, type_examen: str, laboratoire_recommande: str, *, posologie: str ="Aucune", duree_traitement: str ="Aucune", ) -> None:
+        super().__init__(type_examen, posologie, duree_traitement)
+        self._type_examen = type_examen
+        self._laboratoire_recommande = laboratoire_recommande
+
+    def afficher_details(self):
+        return (
+            "Prescription d'examen :\n"
+            f"- Type d'examen : {self._type_examen}\n"
+            f"- laboratoire recommandé : {self._laboratoire_recommande}\n"
+            f"- Posologie : {self._posologie}\n"
+            f"- Durée du traitement : {self._duree_traitement}\n"
+        )
+
+class PrescriptionKinesitherapie(Prescription) :
+
+    def __init__(self, zone_traite, nb_seance, posologie="À définir", duree_traitement="À définir"):
+        super().__init__("Kinesitherapie", posologie, duree_traitement)
+        self._zone_traite = zone_traite
+        self._nb_seance = nb_seance
+
+    def afficher_details(self):
+
+        return (
+            "Prescription Kinesitherapie \n"
+            f"- Zone à traité : {self._zone_traite} \n"
+            f"- Nombre de seance : {self._nb_seance} \n"
+            f"- Posologie : {self._posologie}\n"
+            f"- Durée du traitement : {self._duree_traitement}\n"
+        )
+
+    
+
+
