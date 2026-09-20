5e3cff6fb22b0b6ad87df616bbebc03fbe9ae0f6
MathisPhg <mathis.phg@gmail.com>
2026-09-20 18:59:20 +0200

    feat: ajout travail Clara / Lenny

---
+++ models/patient.py
@@ -0,0 +1,88 @@
+from datetime import date
+from utils.validators import InvalidSecurityNumberError
+ 
+ 
+class Patient:
+    def __init__(self, n_securite_sociale: str, nom: str, prenom: str, date_naissance: date, adresse: str, telephone: str):
+        self.n_securite_sociale = n_securite_sociale
+        self._nom = nom
+        self._prenom = prenom
+        self._date_naissance = date_naissance
+        self._adresse = adresse
+        self._telephone = telephone
+        self.liste_consultations = []
+ 
+ 
+    # ---getters . setters
+    @property
+    def n_securite_sociale(self) -> str:
+        return self._n_securite_sociale
+    @n_securite_sociale.setter
+    def n_securite_sociale(self, numero: str):
+        if not self.verification_n_securite_sociale(numero):
+            raise InvalidSecurityNumberError("Numéro invalide ! Il doit contenir 15 chiffres.")
+        self._n_securite_sociale = numero
+ 
+    @property
+    def telephone(self) -> str:
+        return self._telephone
+    @telephone.setter
+    def telephone(self, telephone: str):
+        self._telephone = telephone
+ 
+    @property
+    def nom(self) -> str:
+        return self._nom
+    @nom.setter
+    def nom(self, nom: str):
+        self._nom = nom
+ 
+    @property
+    def prenom(self) -> str:
+        return self._prenom
+    @prenom.setter
+    def prenom(self, prenom: str):
+        self._prenom = prenom
+ 
+    @property
+    def adresse(self) -> str:
+        return self._adresse
+    @adresse.setter
+    def adresse(self, adresse: str):
+        self._adresse = adresse
+ 
+    @property
+    def date_naissance(self) -> date:
+        return self._date_naissance
+    @date_naissance.setter
+    def date_naissance(self, date_naissance: date):
+        self._date_naissance = date_naissance
+ 
+ 
+ 
+    #-------contraintes consignes
+    @staticmethod
+    def verification_n_securite_sociale(numero: str) -> bool:
+        return isinstance(numero, str) and len(numero) == 15 and numero.isdigit()
+ 
+    @property
+    def age(self) -> int:
+        aujourdhui = date.today()
+        age = aujourdhui.year - self.date_naissance.year
+        if (aujourdhui.month, aujourdhui.day) < (self.date_naissance.month, self.date_naissance.day):
+            age -= 1
+        return age
+ 
+    def to_dict(self) -> dict:
+        return {
+            "n_securite_sociale": self.n_securite_sociale,
+            "nom": self.nom,
+            "prenom": self.prenom,
+            "date_naissance": self.date_naissance.isoformat(),
+            "adresse": self.adresse,
+            "telephone": self.telephone,
+            "consultations": [index.to_dict() for index in self.liste_consultations]
+        }
+ 
+    def __repr__(self) -> str:
+        return f"Patient(n_securite_sociale={self.n_securite_sociale}, nom={self.nom}, prenom={self.prenom}, age={self.age})"
---
+++ services/consultation_service.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from models.consultation import Consultation
+from models.prescription import Prescription
+from utils.decorators import log_action, validate_patient
+from utils.validators import ConsultationNotFoundError
+ 
+ 
+class ConsultationService:
+    def __init__(self, patient_service = None,consultations: list = None):
+        self.patient_service = patient_service
+        self.patients = (patient_service.patients if patient_service is not None else {})
+        self.consultations = consultations if consultations is not None else []
+ 
+ 
+    @log_action("Planification d'une consultation")
+    @validate_patient
+    def planifier_consultation(self, n_securite_sociale: str, date_heure: datetime, nom_medecin: str, motif: str) -> Consultation:
+        patient = self.patients[n_securite_sociale]
+ 
+        consultation = Consultation(date_heure=date_heure, patient=patient, nom_medecin=nom_medecin, motif=motif, statut="planifiée")
+        self.consultations.append(consultation)
+        patient.liste_consultations.append(consultation)
+        return consultation
+ 
+ 
+    @log_action("Affichage liste des consultations à venir")
+    def afficher_consultations_a_venir(self):
+        maintenant = datetime.now()
+        a_venir = [c for c in self.consultations if c.date_heure >= maintenant and c.statut == "planifiée"]
+        if not a_venir:
+            print("Aucune consultation à venir.")
+            return
+        print("\n--- Consultations à venir ---")
+ 
+        for i in a_venir:
+            print(f"{i.date_heure.strftime('%Y-%m-%d %H:%M')} - Patient : {i.patient.nom} {i.patient.prenom} - Dr. {i.nom_medecin} - Motif : {i.motif}")
+ 
+ 
+    @log_action("Changement de statut : Consultation réalisée")
+    def marquer_realisee(self, consultation: Consultation):
+        consultation.marquer_realisee()
+ 
+ 
+    @log_action("Annulation de consultation")
+    def annuler_consultation(self, consultation: Consultation):
+        consultation.annuler()
+ 
+ 
+    @log_action("Ajout d'un diagnostic")
+    def ajouter_diagnostic(self, consultation: Consultation, diagnostic: str):
+        consultation.ajouter_diagnostic(diagnostic)
+ 
+ 
+    @log_action("Ajout de prescription(s)")
+    def ajouter_prescriptions(self, consultation: Consultation, *prescriptions: Prescription):
+        for prescription in prescriptions:
+            consultation.ajouter_prescription(prescription)
